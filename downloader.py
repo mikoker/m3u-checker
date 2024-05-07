ef10b1bbec85980a9533b923865088368a6d9574
mikoker <mikolaj.kolata@gmail.com>
2024-05-07 12:52:42 +0200

    dziala. mam dosc

--- downloader.py
+++ downloader.py
@@ -1,39 +1,26 @@
-import requests, concurrent.futures, random, os
+import requests, concurrent.futures, os, json
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
-def download_files(links, folder):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_link = {executor.submit(download_file, link, folder): link for link in links}
-        for future in concurrent.futures.as_completed(future_to_link):
-            link = future_to_link[future]
-            try:
-                future.result()
-            except Exception as e:
-                print(f"Error with {link}, error: {e}")
-
-def download_file(link, folder):
-    filename = str(random.randint(1, 9999)) + ".m3u"
-    with requests.get(link, headers={"User-Agent": user_agent}, stream=True) as response:
-        response.raise_for_status()
-        with open(f"{folder}/{filename}", "wb") as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
+def download_file(link, filename, path):
+    headers = {
+        "User-Agent": user_agent
+    }
+    response = requests.get(link, headers=headers)
+    with open(os.path.join(path, filename), "wb") as f:
+        f.write(response.content)
 
 def main():
-    files = [
-        "common_links.txt",
-        "links.txt",
-        "attachment_links.txt"
-    ]
-
+    file = input("Enter the file name: ")
+    with open(file, "r") as f:
+        data = json.load(f)
     path = "downloads"
     os.makedirs(path, exist_ok=True)
-
-    for file in files:
-        with open(file, "r") as f:
-            links = f.read().splitlines()
-            download_files(links, path)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for item in data:
+            link = item["link"]
+            filename = item["filename"]
+            executor.submit(download_file, link, filename, path)
         
 if __name__ == "__main__":
     main()
--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-import asyncio, aiohttp, ssl
+import asyncio, aiohttp, json
 from bs4 import BeautifulSoup
 
 headers = {
@@ -14,60 +14,39 @@ async def get_content(url):
         html = await fetch(url, session)
         soup = BeautifulSoup(html, "html.parser")
         page_body = soup.find("div", id="page-body")
-        links = []
-        attachment_links = []  # new list for attachment links
-        last_page_num = 0
-        last_page_link = ""
+        data = []
         if page_body:
-            posts = page_body.find_all("div", class_="post has-profile bg2")
+            posts = page_body.select("div", class_="post has-profile")
             for post in posts:
                 link_element = post.find("a", class_="postlink")
                 if link_element:
-                    links.append(link_element.get("href"))
-                # new code to find attachment links
-                attachments = post.find_all("dl", class_="file")
-                for attachment in attachments:
-                    attachment_link_element = attachment.find("a", class_="postlink")
-                    if attachment_link_element:
-                        attachment_links.append(attachment_link_element.get("href"))
-            pagination = soup.find("div", class_="pagination")
-            if pagination:
-                page_links = pagination.find_all("a", class_="button")
-                if page_links:
-                    last_page_link_element = page_links[-2] # change from -1 to -2
-                    last_page_link = last_page_link_element.get("href")
-                    last_page_num = int(last_page_link_element.text)
-        return last_page_num, last_page_link, links, attachment_links  # return attachment links as well
+                    data.append({"link": link_element.get("href"), "filename": link_element.text})
+        pagination = soup.find("div", class_="pagination")
+        last_page_num = 0
+        last_page_link = ""
+        if pagination:
+            page_links = pagination.find_all("a", class_="button")
+            if page_links:
+                last_page_link_element = page_links[-2]
+                last_page_link = last_page_link_element.get("href")
+                last_page_num = int(last_page_link_element.text)
+        return last_page_num, last_page_link, data
 
 async def main():
-    url = "https://sat-forum.net/viewtopic.php?t=424"
+    filename = input("Enter the file name: ")
+    topic_url = "https://sat-forum.net/viewtopic.php?t=424"
     base_url = "https://sat-forum.net/"
-    page = "&start=" # co 10
-    last_page_num, last_page_link, links, attachment_links = await get_content(url)
-    print(last_page_num)
+    page = "&start="
+    last_page_num, last_page_link, data = await get_content(topic_url)
+    results = set()
+    results.update({(base_url + item["link"].replace("./", ""), item["filename"]) for item in data})
     for i in range(1, last_page_num):
-        #print(i)
-        url = base_url + last_page_link + page + str(i * 10)
-        _, _, new_links, new_attachment_links = await get_content(url)  # get new_attachment_links
-        #print(new_links)
-        links.extend(new_links)
-        attachment_links.extend(new_attachment_links)  # extend attachment_links with new_attachment_links
+        url = topic_url + page + str(i*10)
+        _, _, data = await get_content(url)
+        results.update({(base_url + item["link"].replace("./", ""), item["filename"]) for item in data})
 
-    full_links = [base_url + link.replace("./", "") if link.startswith("./") else link for link in links]
-    full_attachment_links = [base_url + link.replace("./", "") if link.startswith("./") else link for link in attachment_links]  # create full_attachment_links
+    with open(filename, "w") as f:
+        json.dump([{"link": link, "filename": filename} for link, filename in results], f, indent=4)
 
-    for link in full_links:
-        with open("links.txt", "a") as file:
-            file.write(link + "\n")
-
-    for link in full_attachment_links:  # print full_attachment_links
-        with open("attachment_links.txt", "a") as file:
-            file.write(link + "\n")
-
-    common_links = set(full_links).intersection(full_attachment_links)
-
-    for link in common_links:
-        with open("common_links.txt", "a") as file:
-            file.write(link + "\n")
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
